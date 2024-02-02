a6b2eba9a0fc5b0cceeaf1c2c6d79fdd2c7dd6b1
noah9335 <158576177+noah9335@users.noreply.github.com>
2024-02-02 21:59:57 +0000

    Update refined_shapes.py

--- Noah Program/refined_shapes.py
+++ Noah Program/refined_shapes.py
@@ -1,19 +1,14 @@
+# removed: legal disclaimer, Rasp Pi code, irrelavant display sizes, 
+
+
 import time
 
 #import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 
-from PIL import Image
+from PIL import Image #imports python imaging library   
 from PIL import ImageDraw
 from PIL import ImageFont
-
-
-# Raspberry Pi pin configuration:
-#RST = 24
-# Note the following are only used with SPI:
-#DC = 23
-#SPI_PORT = 0
-#SPI_DEVICE = 0
 
 # Beaglebone Black pin configuration:
 RST = 'P9_12'
@@ -22,29 +17,10 @@ DC = 'P9_15'
 SPI_PORT = 1
 SPI_DEVICE = 0
 
-# 128x32 display with hardware I2C:
-#disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
-# 128x64 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
-# Note you can change the I2C address by passing an i2c_address parameter like:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
-
-# Alternatively you can specify an explicit I2C bus number, for example
-# with the 128x32 display you would use:
+# specify an explicit I2C bus number (for 128x32 display size)
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
-
-# 128x32 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# 128x64 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# Alternatively you can specify a software SPI implementation by providing
-# digital GPIO pin numbers for all the required display pins.  For example
-# on a Raspberry Pi with the 128x32 display you might use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
 
 # Initialize library.
 disp.begin()
@@ -89,10 +65,6 @@ x += shape_width+padding
 
 # Load default font.
 font = ImageFont.load_default()
-
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
-#font = ImageFont.truetype('Minecraftia.ttf', 8)
 
 # Write two lines of text.
 draw.text((x, top),    'Hello',  font=font, fill=255)
